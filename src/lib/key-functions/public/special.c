660ddd6885ccc6fea4d2230bcf45754fce18f965
weinstein <jhsw93@gmail.com>
2018-01-31 23:27:29 -0800

    add crtl+key macro fn, make more thumb cluster keys transparent, add tabs to thumb cluster, add a shift key to rhs

--- src/lib/key-functions/public/special.c
+++ src/lib/key-functions/public/special.c
@@ -43,6 +43,11 @@ void kbfun_shift_press_release(void) {
 	kbfun_press_release();
 }
 
+void kbfun_ctrl_press_release(void) {
+  _kbfun_press_release(IS_PRESSED, KEY_RightControl);
+  kbfun_press_release();
+}
+
 /*
  * [name]
  *   Two keys => capslock
